16120f8145e6beb35114510870131fae5912e33a
Petru Bodea <petrubodea94@gmail.com>
2021-09-08 19:45:33 +0300

    angular update to v11

--- sportsevents-ui/src/app/edit-events/edit-events.component.spec.ts
+++ sportsevents-ui/src/app/edit-events/edit-events.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EditEventsComponent } from './edit-events.component';
 
@@ -6,7 +6,7 @@ describe('EditEventsComponent', () => {
   let component: EditEventsComponent;
   let fixture: ComponentFixture<EditEventsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EditEventsComponent ]
     })
--- sportsevents-ui/src/app/edit-users/edit-users.component.spec.ts
+++ sportsevents-ui/src/app/edit-users/edit-users.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EditUsersComponent } from './edit-users.component';
 
@@ -6,7 +6,7 @@ describe('EditUsersComponent', () => {
   let component: EditUsersComponent;
   let fixture: ComponentFixture<EditUsersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EditUsersComponent ]
     })
