5c06d143bc598bc13164f89092f4ef2de96c2fee
Petru Bodea <petrubodea94@gmail.com>
2021-12-08 23:02:41 +0200

    update keycloak version, fix EventsController

--- events-service-spring/src/main/java/com/sportsevents/api/EventsController.java
+++ events-service-spring/src/main/java/com/sportsevents/api/EventsController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ import io.micrometer.core.annotation.Timed;
 @RequestMapping("events")
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+
 public class EventsController {
     @Autowired
     EventService eventService;
@@ -44,7 +46,7 @@ public class EventsController {
 
     @PostMapping
     @Timed(value="create_event.request", histogram=true)
-    public ResponseEntity<Event> createEvent(@Valid EventModel event){
+    public ResponseEntity<Event> createEvent(@RequestBody @Valid EventModel event){
         try{ 
             Optional<Event> opt = eventService.createEvent(event);
             if(opt.isPresent()){
@@ -59,7 +61,7 @@ public class EventsController {
 
     @PatchMapping("/addPlayer")
     @Timed(value="add_player.request", histogram=true)
-    public ResponseEntity addPlayer(@Valid UpdatePlayersModel updatePlayersModel){
+    public ResponseEntity addPlayer(@RequestBody @Valid UpdatePlayersModel updatePlayersModel){
 
         if(eventService.addPlayer(updatePlayersModel)){
             return ResponseEntity.ok().build();
@@ -71,7 +73,7 @@ public class EventsController {
 
     @PatchMapping("/removePlayer")
     @Timed(value="remove_player.request", histogram=true)
-    public ResponseEntity removePlayer(@Valid UpdatePlayersModel updatePlayersModel){
+    public ResponseEntity removePlayer(@RequestBody @Valid UpdatePlayersModel updatePlayersModel){
 
         if(eventService.removePlayer(updatePlayersModel)){
             return ResponseEntity.ok().build();
@@ -83,7 +85,7 @@ public class EventsController {
 
     @PostMapping("/closeEvent")
     @Timed(value="close_event.request", histogram=true)
-    public ResponseEntity closeEvent(@Valid ClosedEventModel closedEventModel){
+    public ResponseEntity closeEvent(@RequestBody @Valid ClosedEventModel closedEventModel){
 
         if(eventService.closeEvent(closedEventModel)){
             return ResponseEntity.ok().build();
--- events-service-spring/src/main/java/com/sportsevents/api/LeaderboardController.java
+++ events-service-spring/src/main/java/com/sportsevents/api/LeaderboardController.java
@@ -7,7 +7,6 @@ import com.sportsevents.api.model.LeaderboardEntryModel;
 import com.sportsevents.service.EventService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
