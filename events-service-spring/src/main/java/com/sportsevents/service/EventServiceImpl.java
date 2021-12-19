e63a142878b6c985f5027dc5e43b0f5d12e981de
Petru Bodea <petrubodea94@gmail.com>
2021-12-19 23:34:36 +0200

    remove unused imports

--- events-service-spring/src/main/java/com/sportsevents/service/EventServiceImpl.java
+++ events-service-spring/src/main/java/com/sportsevents/service/EventServiceImpl.java
@@ -4,11 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.transaction.Transactional;
 
