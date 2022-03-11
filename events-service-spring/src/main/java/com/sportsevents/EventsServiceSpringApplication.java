14db494d6a40c32af681ae19c9e622ec0df505c0
whirl94 <petrubodea94@gmail.com>
2022-03-11 19:10:21 +0200

    update app properties

--- events-service-spring/src/main/java/com/sportsevents/EventsServiceSpringApplication.java
+++ events-service-spring/src/main/java/com/sportsevents/EventsServiceSpringApplication.java
@@ -1,66 +1,11 @@
 package com.sportsevents;
-
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Random;
-
-import com.sportsevents.entity.Event;
-import com.sportsevents.entity.EventStatus;
-import com.sportsevents.entity.EventType;
-import com.sportsevents.entity.EventsRepo;
-import com.sportsevents.service.EventFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class EventsServiceSpringApplication {
 
-	@Autowired
-    private EventsRepo eventsRepo;
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventsServiceSpringApplication.class, args);
 	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void loadInitialData() {
-		eventsRepo.deleteAll();
-		for(int i = 0; i < 300; i++){
-            Event event = null;
-            if(i % 2 == 0){
-                event = new Event.EventBuilder()
-                    .name(EventFactory.PINGPONG_EVENT_NAME)
-                    .duration(EventFactory.PINGPONG_EVENT_DURATION)
-                    .scheduledDate(LocalDateTime.of(2022,Month.FEBRUARY, 22, 12, 30))
-                    .isOutdoors(true)
-                    .location("Bucuresti")
-                    .facilitator("facilitator")
-                    .status(EventStatus.CREATED.toString())
-                    .type(EventType.PINGPONG.toString())
-                    .maxPlayers(EventFactory.PINGPONG_EVENT_MAX_PLAYERS)
-                    .build();
-            } else{
-                event = new Event.EventBuilder()
-                    .name(EventFactory.CHESS_EVENT_NAME)
-                    .duration(EventFactory.CHESS_EVENT_DURATION)
-                    .scheduledDate(LocalDateTime.of(2021,Month.DECEMBER, 1, 12, 30))
-                    .isOutdoors(true)
-                    .location("Bucuresti")
-                    .facilitator("facilitator")
-                    .status(EventStatus.CLOSED.toString())
-                    .type(EventType.CHESS.toString())
-                    .maxPlayers(EventFactory.CHESS_EVENT_MAX_PLAYERS)
-                    .players(new String[]{"gica", "gica2"})
-                    .winner(new Random().nextBoolean() ? "gica" : "gica2")
-                    .closedDate(LocalDateTime.of(2022,Month.DECEMBER, 3, 10, 30))
-                    .build();
-            }
-            eventsRepo.save(event);
-        }
-	}
-
 }
--- events-service-spring/src/main/java/com/sportsevents/entity/InitialData.java
+++ events-service-spring/src/main/java/com/sportsevents/entity/InitialData.java
@@ -6,6 +6,8 @@ import org.springframework.context.event.EventListener;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Random;
+
 import com.sportsevents.service.EventFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +20,13 @@ public class InitialData {
     @EventListener
     public void appReady(ApplicationReadyEvent e) {
         eventsRepo.deleteAll();
-        for(int i = 0; i < 200; i++){
+		for(int i = 0; i < 300; i++){
             Event event = null;
             if(i % 2 == 0){
                 event = new Event.EventBuilder()
                     .name(EventFactory.PINGPONG_EVENT_NAME)
                     .duration(EventFactory.PINGPONG_EVENT_DURATION)
-                    .scheduledDate(LocalDateTime.of(2022,Month.FEBRUARY, 22, 12, 30))
+                    .scheduledDate(LocalDateTime.of(2022,Month.AUGUST, 22, 12, 30))
                     .isOutdoors(true)
                     .location("Bucuresti")
                     .facilitator("facilitator")
@@ -44,14 +46,12 @@ public class InitialData {
                     .type(EventType.CHESS.toString())
                     .maxPlayers(EventFactory.CHESS_EVENT_MAX_PLAYERS)
                     .players(new String[]{"gica", "gica2"})
-                    .winner("gica2")
+                    .winner(new Random().nextBoolean() ? "gica" : "gica2")
                     .closedDate(LocalDateTime.of(2022,Month.DECEMBER, 3, 10, 30))
                     .build();
             }
             eventsRepo.save(event);
         }
-
-		
-        
     }
+
 }
