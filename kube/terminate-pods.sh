ce8dbe67fbc91cc996871d8806f7029be42cb0f4
whirl94 <petrubodea94@gmail.com>
2022-05-24 20:03:00 +0300

    update terminate-pods shell file; add springboot service

--- kube/terminate-pods.sh
+++ kube/terminate-pods.sh
@@ -1,4 +1,14 @@
+if [ "$1" != "quarkus-jvm" ] && [ "$1" != "quarkus-native" ] && [ "$1" != "springboot" ]
+  then
+    echo "Wrong pod argument, possible values: quarkus-jvm, quarkus-native, springboot"
+    exit 1
+fi
 
+if [ $2 != 5 ] && [ $2 != 10 ] && [ $2 != 15 ] && [ $2 != 20 ]
+  then
+    echo "No sleep argument passed, possible values: 5, 10, 15, 20"
+    exit 1
+fi
 
 echo "Starting $1 busybox ... "
 
@@ -10,25 +20,30 @@ sleep 12
 
 echo "Terminating $1 pods ..."
 
-for i in {1..5}
+for i in {1..10}
 do
   POD_TO_KILL=$(kubectl get po --selector "app=$1" --output=name | head -n1)
   
   echo "will kill $POD_TO_KILL"
-    kubectl delete --force $POD_TO_KILL
-  sleep 10
+  kubectl delete --force $POD_TO_KILL
+  # sleep time between restarts
+  sleep $2
 done
 
 if [ -f "ping-$1.log" ] ; then
     rm "ping-$1.log"
 fi
 
-kubectl logs ping-quarkus-jvm > ping-quarkus-jvm.log
+kubectl logs ping-$1 > ping-$1.log
 
-TOTAL_REQUESTS=$(expr $(cat ping-quarkus-jvm.log | grep "Trying to connect to" | wc -l))
-ERRORS=$(expr $(cat ping-quarkus-jvm.log | grep "Connection refused" | wc -l) + $(cat ping-quarkus-jvm.log | grep -i 'timed out' | wc -l) +  $(cat ping-quarkus-jvm.log | grep -i 'peer' | wc -l))
+TOTAL_REQUESTS=$(expr $(cat ping-$1.log | grep "Trying to connect to" | wc -l))
+ERRORS=$(expr $(cat ping-$1.log | grep "Connection refused" | wc -l) + $(cat ping-$1.log | grep -i 'timed out' | wc -l) +  $(cat ping-$1.log | grep -i 'peer' | wc -l))
 
-echo "Total requests: $TOTAL_REQUESTS"
-echo "Number of $1 faults: $ERRORS"
+printf "\n\n\n==========================> $1 run; `date`\n" >> ping-result.log
+printf "==========================> sleep time between pod restarts: $2\n" >> ping-result.log
+echo "Total requests: $TOTAL_REQUESTS" >> ping-result.log
+echo "Number of $1 faults: $ERRORS" >> ping-result.log
 
-kubectl delete -f ping/quarkus-jvm-ping.yml
+kubectl delete -f ping/$1-ping.yml
+
+exit 0
